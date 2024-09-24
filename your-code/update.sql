cd7375579c907fa91e354adac6a5bcd065afd479
sauluve <saul.verdu@hotmail.com>
2024-09-24 16:52:08 +0200

    240924 Lab Intro to MySQL

---
+++ your-code/update.sql
@@ -0,0 +1,4 @@
+UPDATE salespersons SET store_name = "Miami" WHERE salesperson_id = "00005";
+UPDATE customers SET email = "ppicasso@gmail.com" WHERE customer_id = "10001";
+UPDATE customers SET email = "lincoln@us.gov" WHERE customer_id = "20001";
+UPDATE customers SET email = "hello@napoleon.me" WHERE customer_id = "30001";
